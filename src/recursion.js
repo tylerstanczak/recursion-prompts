4b3c24d9ee0b75e76231bb6998294cc8cff8b7d7
tylerstanczak <tylerstanczak@gmail.com>
2019-08-18 11:25:53 -0500

    Define range

--- src/recursion.js
+++ src/recursion.js
@@ -55,6 +55,12 @@ var sumBelow = function(n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  if (x + 1 === y || x - 1 === y || x === y) return [];
+  var min = (x < y) ? x : y;
+  return (min === x) ? [x+1].concat(range(x+1, y)) : [x-1].concat(range(x-1, y));
+  
+
+
 };
 
 // 7. Compute the exponent of a number.

7397c4ea28cf768b7a92fd6b92cab968587138cf
tylerstanczak <tylerstanczak@gmail.com>
2019-08-19 17:18:20 -0500

    Define createArray

--- src/recursion.js
+++ src/recursion.js
@@ -155,6 +155,8 @@ var compareStr = function(str1, str2) {
 // 16. Write a function that accepts a string and creates an array where each letter
 // occupies an index of the array.
 var createArray = function(str) {
+
+  return (str[0]) ? [str[0]].concat(createArray(str.slice(1))) : [];
 };
 
 // 17. Reverse the order of an array

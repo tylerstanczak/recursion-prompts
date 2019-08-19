d092df3d8568c4091058112395b6fa5dae86c8ee
tylerstanczak <tylerstanczak@gmail.com>
2019-08-19 15:52:19 -0500

    Define gcd

--- src/recursion.js
+++ src/recursion.js
@@ -137,8 +137,10 @@ var divide = function(x, y) {
 // http://www.cse.wustl.edu/~kjg/cse131/Notes/Recursion/recursion.html
 // https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
 var gcd = function(x, y) {
+  if (x < 0 || y < 0) return null;
   if (x === 0) return y;
   if (y === 0) return x;
+  return gcd(y, (modulo(x, y)));
 };
 
 // 15. Write a function that compares each character of two strings and returns true if

f92664234db72e9b53a0a3227edcb35712f1a52e
tylerstanczak <tylerstanczak@gmail.com>
2019-08-18 23:22:17 -0500

    Define palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -91,6 +91,10 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  if (string.length === 1) return true;
+  if (string.length === 2) return (string[0] === string[1]);
+  string = string.toUpperCase();
+  return (string[0] === string[string.length-1]) ? palindrome(string.slice(1, string.length-1)) : false;
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the

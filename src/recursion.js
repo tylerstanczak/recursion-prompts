3986ac136eaa08e4c6a1ab13b7b5a58dcbd09241
tylerstanczak <tylerstanczak@gmail.com>
2019-08-19 15:54:42 -0500

    Define compareStr

--- src/recursion.js
+++ src/recursion.js
@@ -148,6 +148,8 @@ var gcd = function(x, y) {
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
 var compareStr = function(str1, str2) {
+  if (str1 === '' && str2 === '') return true;
+  return (str1[0] === str2[0]) ? compareStr(str1.slice(1), str2.slice(1)) : false;
 };
 
 // 16. Write a function that accepts a string and creates an array where each letter

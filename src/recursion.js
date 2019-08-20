305f15dee8887f202c2c12be1a6a374812505858
tylerstanczak <tylerstanczak@gmail.com>
2019-08-19 21:05:10 -0500

    Define rMap

--- src/recursion.js
+++ src/recursion.js
@@ -195,6 +195,8 @@ var countOccurrence = function(array, value) {
 // 21. Write a recursive version of map.
 // rMap([1,2,3], timesTwo); // [2,4,6]
 var rMap = function(array, callback) {
+  if (array.length < 1) return [];
+  return [callback(array[0])].concat(rMap(array.slice(1), callback));
 };
 
 // 22. Write a function that counts the number of times a key occurs in an object.
